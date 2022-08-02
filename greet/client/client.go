e9eae4db036ed56bd7368c931b0e37b187ba38ba
Vikas Sheoran <kkviks@gmail.com>
2022-08-02 10:44:33 +0530

    Add client boilerplate code

---
+++ greet/client/client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect on %s\n", addr)
+		return
+	}
+
+	log.Printf("Connected to %s\n", addr)
+
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("Error closing connection: %v", conn)
+		}
+		log.Println("Connection closed")
+	}(conn)
+
+}
